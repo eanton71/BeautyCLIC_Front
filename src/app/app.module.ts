c2d814307381f10d6f699b16b37dc263ca6aaeec
sualsa <su.alba@hotmail.com>
2023-01-13 13:32:35 +0100

    ** actualizado Header y creado navbar basico **

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FormsModule } from '@angular/forms';
-import { FooterComponent } from './footer/footer.component'; 
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent 
+    FooterComponent,
+    NavbarComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
