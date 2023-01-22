1d597610ff04ae16c8e96a133157eff237aa86a0
sualsa <su.alba@hotmail.com>
2023-01-22 18:15:04 +0100

    header,navbar,contacto,politica enroutado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,31 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HeaderComponent } from './components/shared/header/header.component';
-import { FooterComponent } from './components/shared/footer/footer.component';
-import { NavbarComponent } from './components/shared/navbar/navbar.component';
-import { ContactoComponent } from './components/contacto/contacto.component';
-import { PoliticaprivacidadComponent } from './components/politicaprivacidad/politicaprivacidad.component';
-import { HomeComponent } from './home/home.component';
 
-
-
-const routes: Routes = [
-
-{ path: "home", component:HomeComponent },
-
- { path: 'header', component: HeaderComponent },
- { path: 'footer', component: FooterComponent },
- { path: 'navBar', component: NavbarComponent },
- { path: 'contacto', component: ContactoComponent },
- { path: 'politicaprivacidad', component: PoliticaprivacidadComponent },
-{ path: '', pathMatch: 'full', redirectTo: '/home' },
-{ path: '**', pathMatch: 'full', redirectTo: '/home' },
-];
-
-
-
-
-
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
