16b9865cbb060c7c9e3d58c935b33eb83a9f455d
sualsa <su.alba@hotmail.com>
2023-01-22 18:03:17 +0100

    "componentes header, navbar ,contacto,politica"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './components/shared/header/header.component';
 import { FormsModule } from '@angular/forms';
-import { FooterComponent } from './footer/footer.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { PoliticaprivacidadComponent } from './components/politicaprivacidad/politicaprivacidad.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
@@ -15,6 +17,9 @@ import { NavbarComponent } from './navbar/navbar.component';
     HeaderComponent,
     FooterComponent,
     NavbarComponent,
+    ContactoComponent,
+    PoliticaprivacidadComponent,
+    HomeComponent,
 
   ],
   imports: [
