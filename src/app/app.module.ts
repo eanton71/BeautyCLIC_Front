1884e11b8b5566f86d30a5648a7069d200ec17b6
sualsa <su.alba@hotmail.com>
2023-01-22 18:15:04 +0100

    header,navbar,contacto,politica enroutado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/shared/header/header.component';
+import { HeaderComponent } from './header/header.component';
 import { FormsModule } from '@angular/forms';
-import { FooterComponent } from './components/shared/footer/footer.component';
-import { NavbarComponent } from './components/shared/navbar/navbar.component';
-import { ContactoComponent } from './components/contacto/contacto.component';
-import { PoliticaprivacidadComponent } from './components/politicaprivacidad/politicaprivacidad.component';
-import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
