0aa4e16f1e474fbe5f55d323fda4d713c9396f32
Javier S <jsesen@hotmail.com>
2023-02-06 22:10:40 +0100

    cambios sin finalizar, retroceder 1 la historia

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/shared/header/header.component';
-import { FormsModule } from '@angular/forms';
+
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
 import { FooterComponent } from './components/shared/footer/footer.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { ContactoComponent } from './components/contacto/contacto.component';
 import { PoliticaprivacidadComponent } from './components/politicaprivacidad/politicaprivacidad.component';
 import { HomeComponent } from './home/home.component';
 import { CalendarComponent } from './calendar/calendar.component';
+import { BorrarComponent } from './borrar/borrar.component';
 
 
 
@@ -22,13 +27,16 @@ import { CalendarComponent } from './calendar/calendar.component';
     ContactoComponent,
     PoliticaprivacidadComponent,
     HomeComponent,
-    CalendarComponent
+    CalendarComponent,
+    BorrarComponent
 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/shared/footer/footer.component.ts
+++ src/app/components/shared/footer/footer.component.ts
@@ -1,4 +1,6 @@
+import { ThisReceiver } from '@angular/compiler';
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-footer',
@@ -7,4 +9,77 @@ import { Component } from '@angular/core';
 })
 export class FooterComponent {
 
+  newsForm: FormGroup;
+  newsAsk: boolean;  
+  showAcceptingError: boolean;
+  showEmailError: boolean;
+  avanzado: number;
+  hola: string;
+  accepting: FormControl;
+  email: FormControl;
+  submited: boolean;
+
+  constructor(private fb:FormBuilder){
+    this.email = new FormControl('', [Validators.required, Validators.email]);
+    this.accepting = new FormControl(0,Validators.required);
+
+    /* this.newsForm = this.fb.group({      
+      email:['', [Validators.required, Validators.email]],
+      accepting:[0,Validators.requiredTrue],      
+    })      */
+
+    this.newsForm = new FormGroup({      
+      email: this.email,
+      accepting: this.accepting,      
+    })     
+
+
+    this.newsAsk = true;     
+    this.showAcceptingError = false;     
+    this.showEmailError = false;   
+    
+    this.avanzado = 1;
+    this.hola = "holahola";
+    this.submited = false;
+
+
+  }
+
+  avanza():void{
+    this.avanzado++;
+    console.log('Avanzado', this.avanzado);
+  }
+
+  auto_avanza():void{
+    console.log('auto');
+    setTimeout(()=>{
+      this.avanza();}, 1);
+    } 
+    
+  
+
+  ngOnInit(){
+    this.newsForm.reset();
+    
+  }
+
+  send():void{
+    console.log('en sending');
+    this.submited = true;
+
+    const {email, accepting} = this.newsForm.getRawValue();
+
+    if (this.accepting.errors || this.email.errors) {return; }
+
+    console.log('Accepting es: ', accepting);
+    console.log('EmaIL IS: ', email);
+
+    this.newsForm.reset();
+    this.newsAsk = false;
+    this.showAcceptingError = false;
+    this.showEmailError = false;
+
+  }
+
 }
+
