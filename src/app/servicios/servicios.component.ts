d560f354da3a82d403d3b581a96ff2ef3c5a7a62
eanton71 <eanton71@gmail.com>
2023-02-06 18:26:29 +0100

    actualizado ejemplo cmpponente servicios

--- src/app/servicios/servicios.component.ts
+++ src/app/servicios/servicios.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
 import { Categoria } from 'app/models/categoria';
 import { ServiciosService } from 'app/services/servicios.service';
+import { environment } from '../../environments/environment';
 
-
+//environment.url_public
 @Component({
   selector: 'app-servicios',
   templateUrl: './servicios.component.html',
   styleUrls: ['./servicios.component.scss']
 })
 export class ServiciosComponent {
+  serverimg = environment.url_public;
   categorias: Categoria[] = [];
+  urlimg: string[] = [];
   constructor(private serviciosService: ServiciosService) {
 
   }
