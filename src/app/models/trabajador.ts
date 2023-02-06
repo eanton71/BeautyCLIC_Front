7d93e8aea84aa429193cf5ed76842f4afb94b6cd
eanton71 <eanton71@gmail.com>
2023-02-06 17:44:53 +0100

    modificado interface trabajador

--- src/app/models/trabajador.ts
+++ src/app/models/trabajador.ts
@@ -2,19 +2,7 @@ export interface Trabajador {
     _id: string;
     nombre: string;
     apellidos: string;
-    img: string;
-    email: string;
-    //1 a 5 son lunes a viernes 
-    horario: {
-        "1": [ 
-        ],
-        "2": [ 
-        ],
-        "3": [ 
-        ],
-        "4": [ 
-        ],
-        "5": [ 
-        ]
-    }
+    email: string; 
+    info: "";
+    foto: "";
 }
