2e101b3d5ac8f146a66ee7a4832dbed67d4761ee
Atomico <atomico@alien.com>
2023-02-07 23:57:59 +0100

    Revert "cambios sin finalizar, retroceder 1 la historia"
    This reverts commit 0aa4e16f1e474fbe5f55d323fda4d713c9396f32.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/shared/header/header.component';
-
-import { HttpClientModule } from '@angular/common/http';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-
+import { FormsModule } from '@angular/forms';
 import { FooterComponent } from './components/shared/footer/footer.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { ContactoComponent } from './components/contacto/contacto.component';
 import { PoliticaprivacidadComponent } from './components/politicaprivacidad/politicaprivacidad.component';
 import { HomeComponent } from './home/home.component';
 import { CalendarComponent } from './calendar/calendar.component';
-import { BorrarComponent } from './borrar/borrar.component';
 
 
 
@@ -27,16 +22,13 @@ import { BorrarComponent } from './borrar/borrar.component';
     ContactoComponent,
     PoliticaprivacidadComponent,
     HomeComponent,
-    CalendarComponent,
-    BorrarComponent
+    CalendarComponent
 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule,
-    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
