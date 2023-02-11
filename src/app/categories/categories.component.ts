8df1f1683081efb84062a4c71b77eb6b82c0ccfe
mlinaresweb <ghaedesigns@gmail.com>
2023-02-11 14:25:38 +0100

    categorias

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Categoria } from 'app/models/categoria';
+import { Servicio } from 'app/models/servicio';
+import { ServiciosService } from 'app/services/servicios.service';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-categories',
@@ -6,5 +10,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./categories.component.scss']
 })
 export class CategoriesComponent {
+  serverimg = environment.url_public;
+  categorias: Categoria[] = [];
+  servicios: Servicio[] = [];
+  urlimg: string[] = [];
+  mostrarCategorias = true;
 
+  constructor(private serviciosService: ServiciosService) {
+
+  }
+
+  ngOnInit() {
+    this.getCategorias();
+  }
+
+  private getCategorias(): void {
+    this.serviciosService.getCategorias().subscribe(res => this.categorias = res);
+  }
+
+  public getServicios(id: string): void{
+    this.mostrarCategorias = false;
+    this.serviciosService.getServicios(id).subscribe(res => this.servicios = res);
+  }
 }
