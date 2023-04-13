5edf4453852d68291a1267e70306dfa38cf7e9bf
eanton71 <eanton71@gmail.com>
2023-04-13 22:52:22 +0200

    google analitycs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,48 +1,28 @@
-import { DOCUMENT } from '@angular/common';
-import { Component, Inject } from '@angular/core';
-import { Title } from '@angular/platform-browser';
-import { ActivatedRoute, NavigationEnd, Router, RouterState } from '@angular/router';
-declare var gtag:Function;
+
+import { Component } from '@angular/core'; 
+import { NavigationEnd, Router } from '@angular/router';
+
+declare const gtag: Function;
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
-  constructor(
-    private router: Router,
-    private titleService: Title,
-    @Inject(DOCUMENT) private document: Document
-  ) {
-    this.handleRouteEvents();
-  }
-
-  handleRouteEvents() {
+  title = 'BeautyFront';
+  constructor(private router: Router) {
     this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
-        const title = this.getTitle(this.router.routerState, this.router.routerState.root).join('-');
-        this.titleService.setTitle(title);
-        gtag('event', 'page_view', {
-          page_title: title,
-          page_path: event.urlAfterRedirects,
-          page_location: this.document.location.href
-        })
+        console.log(event.urlAfterRedirects);
+        gtag('config', 'G-GQRM8Y269W',
+          {
+            'page_path': event.urlAfterRedirects,
+          }
+        );
       }
     });
   }
-
-  getTitle(state: RouterState, parent: ActivatedRoute): string[] {
-    const data = [];
-    if (parent && parent.snapshot.data && parent.snapshot.data['title']) {
-      data.push(parent.snapshot.data['title']);
-    }
-    if (state && parent && parent.firstChild) {
-      data.push(...this.getTitle(state, parent.firstChild));
-    }
-    return data;
-  }
 }
 
-
-//title = 'BeautyFront';
